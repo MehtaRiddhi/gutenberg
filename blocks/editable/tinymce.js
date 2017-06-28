11221e84b718cac64c801d4de71e0539d1a2078b
Riad Benguella <benguella@gmail.com>
2017-06-28 13:15:09 +0100

    Blocks: Adding the block's classname as prop to use in the `edit`

--- blocks/editable/tinymce.js
+++ blocks/editable/tinymce.js
@@ -3,6 +3,7 @@
  */
 import tinymce from 'tinymce';
 import { isEqual } from 'lodash';
+import classnames from 'classnames';
 
 /**
  * WordPress dependencies
@@ -76,7 +77,7 @@ export default class TinyMCE extends Component {
 	}
 
 	render() {
-		const { tagName = 'div', style, defaultValue, label } = this.props;
+		const { tagName = 'div', style, defaultValue, label, className } = this.props;
 
 		// If a default value is provided, render it into the DOM even before
 		// TinyMCE finishes initializing. This avoids a short delay by allowing
@@ -90,7 +91,7 @@ export default class TinyMCE extends Component {
 			ref: ( node ) => this.editorNode = node,
 			contentEditable: true,
 			suppressContentEditableWarning: true,
-			className: 'blocks-editable__tinymce',
+			className: classnames( className, 'blocks-editable__tinymce' ),
 			style,
 			'aria-label': label,
 		}, children );
